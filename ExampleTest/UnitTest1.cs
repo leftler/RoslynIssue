7e2d644c2a6e9cf1bdbb6da137e4db698ed8580c
Scott R. Chamberlain <srchamberlain@impromed.com>
2016-10-24 14:45:23 -0500

    Code checkin

---
+++ ExampleTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestFailingDummyProgramWithDefaultSettings()
+        {
+            var ws = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create();
+            var solution = await ws.OpenSolutionAsync(@"..\FailingProgram\DummyProgram.sln");
+            Assert.AreEqual(1, solution.Projects.Count());
+
+            foreach (var project in solution.Projects)
+            {
+                var comp = await project.GetCompilationAsync();
+                Assert.IsTrue(comp.References.Any(x=>x.Display.EndsWith(@"\mscorlib.dll")), "Reference to mscorlib.dll not found found");
+            }
+        }
+
+        [TestMethod]
+        public async Task TestFailingDummyProgramWithx86BuildConfig()
+        {
+            var ws = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create();
+            var solution = await ws.OpenSolutionAsync(@"..\FailingProgram\DummyProgram.sln");
+            Assert.AreEqual(1, solution.Projects.Count());
+
+            foreach (var project in solution.Projects)
+            {
+                var modifiedProject = project.WithCompilationOptions(project.CompilationOptions.WithPlatform(Platform.X86));
+                var comp = await project.GetCompilationAsync();
+                Assert.IsTrue(comp.References.Any(x => x.Display.EndsWith(@"\mscorlib.dll")), "Reference to mscorlib.dll not found found");
+            }
+        }
+
+        [TestMethod]
+        public async Task TestWorkingDummyProgramWithDefaultSettings()
+        {
+            var ws = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create();
+            var solution = await ws.OpenSolutionAsync(@"..\WorkingProgram\DummyProgram.sln");
+            Assert.AreEqual(1, solution.Projects.Count());
+
+            foreach (var project in solution.Projects)
+            {
+                
+                var comp = await project.GetCompilationAsync();
+                Assert.IsTrue(comp.References.Any(x => x.Display.EndsWith(@"\mscorlib.dll")), "Reference to mscorlib.dll not found found");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+This test demonstrates the issue of no valid default project config causes issues in Rosyln with project references.
+
+The line `<Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>` in \FailingProgram\DummyProgram.csproj is causing `Compilation.References` to not include the system libraries associated with the project when no property group targets the `AnyCPU` profile.
+
+To create a new project that fails the test:
+ - Open Configuration Manager
+ - Under "Active Solution Platform" chose `<New...>`
+ - Create a new platform targeting `x86` with "Crate new project platforms" checked.
+ - Under "Active Solution Platform" chose `<Edit...>`
+ - Delete `AnyCPU`
+ - Under "Platform" for DummyProgram choose `<Edit...>`
+ - Delete `AnyCPU`
+ Be sure to delete the project level AnyCPU entries along with the solution level entries or the tests will not show the problem.
+ 
+Workaround to fix the issue
+ - Edit \WorkingProgram\DummyProgram.csproj with notepad
+ - Change `<Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>` to `<Platform Condition=" '$(Platform)' == '' ">x86</Platform>`
+ 
+ 
